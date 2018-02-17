60feaaa650847b0e846a3f25f5797ba2c8024fd3
Ariel Lin <arielin@cs.washington.edu>
2018-02-17 15:13:27 -0800

    fix exports in Enums

--- src/Enums.js
+++ src/Enums.js
@@ -1,21 +1,18 @@
-const AccountType = {
+export const AccountType = {
 	SCHOOL: "school",
 	DONATING_AGENCY_MEMBER: "donating_agency_member",
 	RECEIVING_AGENCY : "receiving_agency",
 	DELIVERER_GROUP: "deliverer_group"
-}
-export default AccountType;
+};
 
-const RequestDurationType = {
+export const RequestDurationType = {
 	DATE: "date",  // an end date
 	RECUR: "num_recurrences"  // number of recurrences
-}
-export default RequestDurationType;
+};
 
-const RequestRepeatType = {
+export const RequestRepeatType = {
     WEEKLY: "weekly",
     BIWEEKLY: "biweekly", // every other week
     MONTHLY: "monthly",
     // TODO Nth weekday of month
 };
-export default RequestRepeatType;
