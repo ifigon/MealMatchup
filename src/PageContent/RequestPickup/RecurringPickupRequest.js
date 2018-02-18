e61fdff65dc74d62197a67df90521ae4bce6d2f2
Ariel Lin <arielin@cs.washington.edu>
2018-02-17 22:57:09 -0800

    [backend][RequestPickup] rebase + add notes for handoff to joyce for frontend

--- src/PageContent/RequestPickup/RecurringPickupRequest.js
+++ src/PageContent/RequestPickup/RecurringPickupRequest.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import * as firebase from 'firebase';
-import RepeatType from '../../Enums.js';
+// TODO: after app_setup is merged
+// import firebase from '../../FirebaseConfig.js';
+import { RequestRepeatType, RequestDurationType } from '../../Enums.js';
 
 class RecurringPickupRequest extends Component {
 
@@ -17,16 +18,27 @@ class RecurringPickupRequest extends Component {
 
         var raUid = event.target.receiveingAgency.value;
         if (!raUid) raUid = null;
+        
         var dgUid = event.target.delivererGroup.value;
         if (!dgUid) dgUid = null;
 
+        var durationValue = null;
+        if (event.target.durationType.value === RequestDurationType.DATE) {
+            durationValue = event.target.endDate.value;
+        } else {
+            durationValue = event.target.numRecurrences.value;
+        }
+
         // create DeliveryRequest object
         var deliveryRequest = {
-            startDate: new Date(event.target.startDate.value),
-            // TODO end 
+            startDate: event.target.startDate.value,
+            duration: {
+                type: event.target.durationType.value,
+                value: durationValue
+            },
             repeats: event.target.repeats.value,
-            startTime: event.target.startTime.value, // TODO?
-            endTime: event.target.endTime.value, // TODO?
+            startTime: event.target.startTime.value,
+            endTime: event.target.endTime.value,
             primaryContact: event.target.primaryContact.value,
             notes: event.target.notes.value,
             receiveingAgency: {
@@ -54,23 +66,25 @@ class RecurringPickupRequest extends Component {
     render() {
         return (
             <form onSubmit={this.submitRequest}>
+                {/* TODO: everything should be required except for notes, and agencies involved */}
                 <label>
                     Start Date
-                    {/* TODO: update/save weekday chosen*/}
                     <input type="date" name="startDate" /><br/>
-                    <input type="radio" name="endType" value="recur" />Recur
-                        <input type="number" name="numReccurences" />times<br/>
-                    <input type="radio" name="endType" value="date" />End by
-                        <input type="date" name="endDate" />
+                    <input type="radio" name="durationType" value={RequestDurationType.RECUR} />
+                        Recur<input type="number" name="numRecurrences" />times<br/>
+                    <input type="radio" name="durationType" value={RequestDurationType.DATE} />
+                        End by<input type="date" name="endDate" />
+                        {/* TODO check if end date is after start date? */}
                 </label><br/>
                 <label>
                     Repeats
                     <select name="repeats">
-                        <option value={RepeatType.WEEKLY}>Weekly</option>
-                        <option value={RepeatType.BIWEEKLY}>Every other week</option>
-                        <option value={RepeatType.MONTHLY}>Monthly</option>
-                        {/* TODO populate based on start date*/}
-                        {/* <option value={RepeatType.??}>Monthly, on the ith of X</option> */}
+                        <option value={RequestRepeatType.WEEKLY}>Weekly</option>
+                        <option value={RequestRepeatType.BIWEEKLY}>Every other week</option>
+                        {/* TODO warning if not every month in the range has this date */}
+                        <option value={RequestRepeatType.MONTHLY}>Monthly</option>
+                        {/* (TODO SPR18) Nth Weekday of Month */}
+                        {/* <option value={RequestRepeatType.??}>Monthly, on the ith of X</option> */}
                     </select>
                 </label><br/>
                 <label>
@@ -87,6 +101,7 @@ class RecurringPickupRequest extends Component {
                 <label>
                     End Time
                     <input type="time" name="endTime" />
+                    {/* TODO check end time is later than start time? */}
                 </label><br/>
                 <label>
                     Pickup Notes
