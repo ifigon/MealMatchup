1f8af026e86fe98866a7c9fc5311428f80ebfe4c
huangjoyce3 <jhuang3@uw.edu>
2018-02-19 16:03:30 -0800

    basic FoodLogs filter setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import "typeface-roboto";
 import NavBar from './PageLayout/Navigation/NavBar.js';
 import PageHeader from './PageLayout/PageHeader.js';
+import logo from './icons/temp-logo.svg'
+import FoodLogsContainer from './PageContent/FoodLogs/FoodLogsContainer';
 
 // The main entry page to load when user is not signed in.
 // Currently (win18), it is just the first page of sign in/up (select account type).
@@ -14,9 +16,10 @@ class App extends Component {
     return (
       <div className="">
         <header className="">
-          <PageHeader value={"Phi Sigma Rho"}></PageHeader>
+          <PageHeader title={"Seattle Gospel Union Mission"} logo={logo}></PageHeader>
         </header>
         <NavBar></NavBar>
+        <FoodLogsContainer></FoodLogsContainer>
       </div>
     );
   }
