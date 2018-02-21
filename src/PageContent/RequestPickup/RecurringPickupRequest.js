b324ed1863b3c0b61bac145af42e1a1d02dfadeb
huangjoyce3 <jhuang3@uw.edu>
2018-02-21 15:55:10 -0800

    style + css changes

--- src/PageContent/RequestPickup/RecurringPickupRequest.js
+++ src/PageContent/RequestPickup/RecurringPickupRequest.js
@@ -2,20 +2,18 @@ import React, {Component} from 'react';
 // TODO: after app_setup is merged
 // import firebase from '../../FirebaseConfig.js';
 import { RequestRepeatType, RequestDurationType } from '../../Enums.js';
+import './RequestPickup.css';
 
 class RecurringPickupRequest extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {};
-
+        this.state = {}; 
         this.submitRequest = this.submitRequest.bind(this);
     }
 
     submitRequest(event) {
         event.preventDefault();
-
         var raUid = event.target.receiveingAgency.value;
         if (!raUid) raUid = null;
         
@@ -61,70 +59,95 @@ class RecurringPickupRequest extends Component {
         // console.log("Added to DB!");
     }
 
-
-
     render() {
         return (
-            <form onSubmit={this.submitRequest}>
-                {/* TODO: everything should be required except for notes, and agencies involved */}
-                <label>
-                    Start Date
-                    <input type="date" name="startDate" /><br/>
-                    <input type="radio" name="durationType" value={RequestDurationType.RECUR} />
-                        Recur<input type="number" name="numRecurrences" />times<br/>
-                    <input type="radio" name="durationType" value={RequestDurationType.DATE} />
-                        End by<input type="date" name="endDate" />
-                        {/* TODO check if end date is after start date? */}
-                </label><br/>
-                <label>
-                    Repeats
-                    <select name="repeats">
-                        <option value={RequestRepeatType.WEEKLY}>Weekly</option>
-                        <option value={RequestRepeatType.BIWEEKLY}>Every other week</option>
-                        {/* TODO warning if not every month in the range has this date */}
-                        <option value={RequestRepeatType.MONTHLY}>Monthly</option>
-                        {/* (TODO SPR18) Nth Weekday of Month */}
-                        {/* <option value={RequestRepeatType.??}>Monthly, on the ith of X</option> */}
-                    </select>
-                </label><br/>
-                <label>
-                    Primary Contact
-                    <select name="primaryContact">
-                        {/*TODO populate based on DA members */}
-                        <option value="<member1_uid>">Member 1 Name</option>
-                    </select>
-                </label><br/>
-                <label>
-                    Start Time
-                    <input type="time" name="startTime" />
-                </label><br/>
-                <label>
-                    End Time
-                    <input type="time" name="endTime" />
-                    {/* TODO check end time is later than start time? */}
-                </label><br/>
-                <label>
-                    Pickup Notes
-                    <textarea name="notes" />
-                </label><br/>
-                <label>
-                    Student Group
-                    <select name="delivererGroup" defaultValue="">
-                        <option value="">Select</option>
-                        {/* TODO: populate based on directory */}
-                        <option value="<dg1_uid>">Group 1 Name</option>
-                    </select>
-                </label><br/>
-                <label>
-                    Shelter
-                    <select name="receiveingAgency" defaultValue="">
-                        <option value="">Select</option>
-                        {/* TODO: populate based on directory */}
-                        <option value="<ra1_uid>">Shelter 1 Name</option>
-                    </select>
-                </label><br/>
-                <input type="submit" value="Done" />
-            </form>
+            <div className="form">
+                <form onSubmit={this.submitRequest}>
+                    {/* TODO: everything should be required except for notes, and agencies involved */}
+                    <div className="info">
+                        <p id="form-heading">Schedule Recurring Pickup</p>
+                        <span className="flex">
+                            <span className="grid">
+                                <label>Start Date  <span className="red">*</span></label><br/>
+                                <input type="date" name="startDate" /><br/>
+                            </span>
+                            <span className="grid">
+                                <label className="smaller">
+                                    <input type="radio" name="durationType" value={RequestDurationType.RECUR} />
+                                    {/* <span className="radio" name="durationType" value={RequestDurationType.RECUR}/> */}
+                                    Recur
+                                    <input type="number" name="numRecurrences" />times<br/>
+                                </label >
+                                <label className="smaller">
+                                    <input type="radio" name="durationType" value={RequestDurationType.DATE} />
+                                    End by
+                                    <input type="date" name="endDate" />
+                                    {/* TODO check if end date is after start date? */}
+                                </label>
+                                <br/>
+                            </span>
+                        </span>
+                        <span className="flex">
+                            <span className="grid">
+                                <label>Repeats <span className="red">*</span></label><br/>
+                                <select name="repeats">
+                                    <option value={RequestRepeatType.WEEKLY}>Weekly</option>
+                                    <option value={RequestRepeatType.BIWEEKLY}>Every other week</option>
+                                    {/* TODO warning if not every month in the range has this date */}
+                                    <option value={RequestRepeatType.MONTHLY}>Monthly</option>
+                                    {/* (TODO SPR18) Nth Weekday of Month */}
+                                    {/* <option value={RequestRepeatType.??}>Monthly, on the ith of X</option> */}
+                                </select><br/>
+                            </span>
+                            <span className="grid">
+                                <label>Primary Contact <span className="red">*</span></label><br/>
+                                <select name="primaryContact">
+                                    {/*TODO populate based on DA members */}
+                                    <option value="<member1_uid>">Member 1 Name</option>
+                                </select><br/>
+                            </span>
+                        </span> 
+                        <span className="flex">
+                            <span className="grid">
+                                <label>Start Time  <span className="red">*</span></label><br/>
+                                <input type="time" name="startTime"/>
+                            </span>
+                            <span className="grid">
+                                <label>End Time  <span className="red">*</span></label><br/>
+                                <input type="time" name="endTime" />
+                                {/* TODO check end time is later than start time? */}
+                            </span>
+                        </span>
+                        <span className="grid">
+                            <p id="form-heading">Notes for Pickup</p>
+                            <textarea name="notes" />
+                        </span>
+                        <p id="form-heading">Agencies involved</p>
+                        <span className="flex">
+                            <span className="grid">
+                                <label>Student Group</label><br/>
+                                <select name="delivererGroup" defaultValue="">
+                                    <option value="">Select</option>
+                                    {/* TODO: populate based on directory */}
+                                    <option value="<dg1_uid>">Group 1 Name</option>
+                                </select>
+                            </span>
+                            <span className="grid">
+                                <label>Shelter</label><br/>
+                                <select name="receiveingAgency" defaultValue="">
+                                    <option value="">Select</option>
+                                    {/* TODO: populate based on directory */}
+                                    <option value="<ra1_uid>">Shelter 1 Name</option>
+                                </select>
+                            </span>
+                        </span>
+                        <div className="buttons">
+                            <input type="submit" value="Done" /> 
+                            <input type="submit" value="Cancel" />
+                        </div>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
