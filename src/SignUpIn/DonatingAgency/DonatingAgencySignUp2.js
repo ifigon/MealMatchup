a095ab3397dcd67d2d1e6d734e3a26615747da50
Zubin Chopra <zubinc@uw.edu>
2018-02-28 22:53:24 -0800

    Cleaned up code

--- src/SignUpIn/DonatingAgency/DonatingAgencySignUp2.js
+++ src/SignUpIn/DonatingAgency/DonatingAgencySignUp2.js
@@ -4,7 +4,6 @@ let ReactDOM = require('react-dom');
 class DonatingAgencySignUp2 extends Component {
     constructor(props) {
         super(props);
-        console.log(props);
         this.nextStep = this.nextStep.bind(this);
     }
     render() {
