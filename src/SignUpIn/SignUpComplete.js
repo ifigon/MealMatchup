8381203bfe35a0a70e2b744e6757399350fb6b51
sarahfeldmann <sarahf95@gmail.com>
2018-03-01 00:32:39 -0800

    fix build

--- src/SignUpIn/SignUpComplete.js
+++ src/SignUpIn/SignUpComplete.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 
 class SignUpComplete extends Component {
-    constructor(props){
-        super(props);
-    }
     render() {
         return (
             <div className="signup-complete-wrapper">
