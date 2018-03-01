d42b24031e1e5b1a7d9859564d6bc9d526fea05d
sarahfeldmann <sarahf95@gmail.com>
2018-03-01 12:51:21 -0800

    fix build

--- src/SignUpIn/SignIn.js
+++ src/SignUpIn/SignIn.js
@@ -2,9 +2,6 @@
 import React, { Component } from 'react'
 
 class SignUpIn extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div className="login-wrapper">
