ccb65aa420b50446335bf6ac282961ba566e04a2
Zubin Chopra <zubinc@uw.edu>
2018-03-09 10:53:25 -0800

    Fixed styling issues for editing Account Settings.

--- src/PageContent/Settings/EditingAccountManager.js
+++ src/PageContent/Settings/EditingAccountManager.js
@@ -7,23 +7,33 @@ class EditingAccountManager extends Component {
             <div className="scs-0">
                 <div className="scs-0-content scs-1-content">
                     <h5>Account Manager Details</h5>
-                    <div className="amd-details amd-details-1">
-                        <div className="amd-details-child">
-                            <label className="label-component details">Student 1</label><input name="name" type="text" className="form-input" /><br />
-                            <h6>{this.props.coordinator.name}</h6>
-                            <h6>{this.props.coordinator.position}</h6>
-                            <h6>{this.props.coordinator.email}</h6>
-                            <h6>{this.props.coordinator.phone}</h6>
+
+                     <div className="editing-box">
+                        <div className="editing-child-1">
+                            <label className="label-component details">Name</label><br /><br />
+                            <label className="label-component details">Title/Position</label><br /><br />
+                            <label className="label-component details">Email</label><br /><br />
+                            <label className="label-component details">Phone Number</label><br /><br />
                         </div>
-                        <div className="amd-details-child">
-                            <h6>Recieve Notifications via</h6>
-                            {this.props.smsNotif ? <h6>SMS/Text Message</h6> : <div/>}
-                            {this.props.emailNotif ? <h6>Email</h6> : <div/>}
+                        
+                        <div className="editing-child-2 amd-editing-2">
+                            <input name="name" type="text" className="form-input" placeholder={this.props.coordinator.name} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.coordinator.position} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.coordinator.email} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.coordinator.phone} /><br /><br />
+                        </div>
+
+                        <div className="editing-child-3">
+                            Recieve Notifications via<br />
+                            <input type="radio" name="smsNotif" value="smsNotif" defaultChecked={this.props.smsNotif}/><label className="label-component details">SMS/Text Message</label><br />
+                            <input type="radio" name="emailNotif" value="emailNotif" defaultChecked={this.props.emailNotif}/><label className="label-component details">Email</label>
                         </div>
                     </div>
+
                     <div className="amd-edit amd-edit-1">
                         <button type="button" className="form-button" id="confirm-button" onClick={this.props.handleAccSave}>Save</button>
                     </div>
+
                 </div>
             </div>
         );
--- src/PageContent/Settings/EditingOrganization.js
+++ src/PageContent/Settings/EditingOrganization.js
@@ -7,16 +7,33 @@ class EditingOrganization extends Component {
             <div className="scs-0">
                 <div className="scs-0-image">
                 </div>
-                <div className="scs-0-content">
+                <div className="scs-0-content scs-1-editing">
                     <h5>Organization Details</h5>
-                    <label className="label-component details">Email</label><input name="name" type="text" className="form-input" placeholder={this.props.email} /><br />
-                    <label className="label-component details">Password</label><input name="name" type="text" className="form-input" placeholder={this.props.password} /><br />
-                    <div className="scs-spacing" />
-                    <label className="label-component details">Street 1</label><input name="name" type="text" className="form-input" placeholder={this.props.address.street1} /><br />
-                    <label className="label-component details">Street 2</label><input name="name" type="text" className="form-input" placeholder={this.props.address.street2} /><br />
-                    <label className="label-component details">City</label><input name="name" type="text" className="form-input" placeholder={this.props.address.city} /><br />
-                    <label className="label-component details">State</label><input name="name" type="text" className="form-input" placeholder={this.props.address.state} /><br />
-                    <label className="label-component details">Zip</label><input name="name" type="text" className="form-input" placeholder={this.props.address.zip} /><br />
+
+                    <div className="editing-box">
+                        <div className="editing-child-1">
+                            <label className="label-component details">Email</label><br /><br />
+                            <label className="label-component details">Password</label><br /><br />
+                            <div className="scs-spacing" />
+                            <label className="label-component details">Street 1</label><br /><br />
+                            <label className="label-component details">Street 2</label><br /><br />
+                            <label className="label-component details">City</label><br /><br />
+                            <label className="label-component details">State</label><br /><br />
+                            <label className="label-component details">Zip</label>
+                        </div>
+                        
+                        <div className="editing-child-2">
+                            <input name="name" type="text" className="form-input" placeholder={this.props.email} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.password} /><br /><br />
+                            <div className="scs-spacing" />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.address.street1} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.address.street2} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.address.city} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.address.state} /><br /><br />
+                            <input name="name" type="text" className="form-input" placeholder={this.props.address.zip} /><br />
+                        </div>
+                    </div>
+
                     <div className="scs-spacing-lg" />
                     <div className="amd-edit amd-edit-1">
                         <button type="button" className="form-button" id="confirm-button" onClick={this.props.handleOrgSave}>Save</button>
