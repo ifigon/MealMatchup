dcf501fb3ee1018d48c754a7a8b211c040f2bb47
Ariel Lin <arielin@cs.washington.edu>
2018-03-15 17:53:03 -0700

    change how enums are exported to support imports from cloud functions

--- src/Enums.js
+++ src/Enums.js
@@ -1,4 +1,8 @@
-export const AccountType = {
+// Disable undeclared variable check on 'exports'
+/*global exports:true*/
+/*eslint no-undef: "error"*/
+
+exports.AccountType = {
     UMBRELLA: 'umbrella',
     DONATING_AGENCY_MEMBER: 'donating_agency_member',
     RECEIVING_AGENCY : 'receiving_agency',
@@ -8,11 +12,11 @@ export const AccountType = {
 // For now (as of 3/5/2018), the only umbrella
 // account type is school (eg UW). In the future,
 // there could be other types such as corporate.
-export const UmbrellaType = {
+exports.UmbrellaType = {
     SCHOOL: 'school',
 };
 
-export const PageContent = {
+exports.PageContent = {
     ASSIGN_VOLUNTEERS: 'Assign Volunteers',
     CALENDAR: 'Calendar',
     DIRECTORY: 'Directory',
@@ -21,19 +25,19 @@ export const PageContent = {
     SETTINGS: 'Settings'
 };
 
-export const RequestDurationType = {
+exports.RequestDurationType = {
     DATE: 'date',  // an end date
     RECUR: 'num_recurrences'  // number of recurrences
 };
 
-export const RequestRepeatType = {
+exports.RequestRepeatType = {
     WEEKLY: 'weekly',
     BIWEEKLY: 'biweekly', // every other week
     MONTHLY: 'monthly',
     // TODO Nth weekday of month
 };
 
-export const NotificationType = {
+exports.NotificationType = {
     NEW_ACCOUNT: 'new_account',
     RECURRING_PICKUP_REQUEST: 'recurring_pickup_request',
     RECURRING_PICKUP_CONFIRMED: 'recurring_pickup_confirmed',
