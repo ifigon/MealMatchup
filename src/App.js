60b45542c6552ceee675c4289af37748d5d21285
sarahfeldmann <sarahf95@gmail.com>
2018-03-22 09:12:23 -1000

    small changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import firebase, { auth } from './FirebaseConfig.js';
-import { PageContent } from './Enums.js';
-import PageContainer from './PageContainer.js';
-import 'typeface-roboto';
+import { PageContent } from "./Enums.js";
+import PageContainer from "./PageContainer.js";
+import "typeface-roboto";
 // import SignUpInController from './SignUpIn/SignUpInController.js'
 
 // The main entry page to load when user is not signed in.
@@ -11,63 +11,60 @@ import 'typeface-roboto';
 // For now, rendering sign up/in components
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      // TODO: a hack to prevent showing logged out page first.. better way?
+      authenticated: true,
+      account: "Sarah"
+    };
+  }
 
-        this.state = {
-            // TODO: a hack to prevent showing logged out page first.. better way?
-            authenticated: true,
-            account: 'Sarah'
-        };
-    }
+  // componentDidMount() {
+  //     // check whether user is logged in
+  //     auth.onAuthStateChanged(function(user) {
+  //         if (user) {
+  //             // grab user's account object
+  //             var accountRef = firebase.database().ref('accounts').child(user.uid);
+  //             accountRef.once('value').then(function(snapshot) {
+  //                 this.setState({
+  //                     authenticated: true,
+  //                     account: snapshot.val()
+  //                 });
+  //             }.bind(this));
+  //         } else {
+  //             this.setState({
+  //                 authenticated: true,
+  //                 account: null
+  //             });
+  //         }
+  //     }.bind(this));
+  // }
 
-    // componentDidMount() {
-    //     // check whether user is logged in
-    //     auth.onAuthStateChanged(function(user) {
-    //         if (user) {
-    //             // grab user's account object
-    //             var accountRef = firebase.database().ref('accounts').child(user.uid);
-    //             accountRef.once('value').then(function(snapshot) {
-    //                 this.setState({
-    //                     authenticated: true,
-    //                     account: snapshot.val()
-    //                 });
-    //             }.bind(this));
-    //         } else {
-    //             this.setState({
-    //                 authenticated: true,
-    //                 account: null
-    //             });
-    //         }   
-    //     }.bind(this));
-    // }
-
-    render() {
-        return (
-            <div className="">
-                {/* {this.state.authenticated ?
+  render() {
+    return (
+      <div className="">
+        {/* {this.state.authenticated ?
                     (this.state.account ?
-                        /* Show Calendar page if user is logged in */ }        
-                <PageContainer 
-                    account={this.state.account}
-                    content={PageContent.CALENDAR}>
-                </PageContainer>
-            </div>
+                        /* Show Calendar page if user is logged in */}
+        <PageContainer
+          account={this.state.account}
+          content={PageContent.CALENDAR}
+        />
+      </div>
 
-        /* :
+      /* :
                         </PageContainer>
                         :
                         <SignUpInController/>
                     )
                     :
                     /* Show blank page if initial authentication hasn't finished */
-        // <div></div>
-        //}
-
-           
-        );
-    }
+      // <div></div>
+      //}
+    );
+  }
 }
 
 export default App;
