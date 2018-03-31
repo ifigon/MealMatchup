07ccbd5c0ced26083d9bc1389c72fc4a7eed9e3f
huangjoyce3 <jhuang3@uw.edu>
2018-03-31 15:29:49 -0700

    add mobile router

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import 'typeface-roboto';
 import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import MobileDelivery from './MobileDelivery/MobileDelivery';
 
 ReactDOM.render(
     <Router>
         <div>
             <Route exact path="/" component={App} />
             {/* TODO: Mobile route */}
+            <Route exact path="/mobile" component={MobileDelivery} />
         </div>
     </Router>,
     document.getElementById('root')
