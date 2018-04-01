e597bd18e1cedc8c735690c5d212f132dabca414
sarahfeldmann <sarahf95@gmail.com>
2018-04-01 01:33:56 -0700

    add future/past css

--- src/PageContent/Calendar/EventCard.js
+++ src/PageContent/Calendar/EventCard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './EventCard.css';
 import green_truck from '../../icons/green_truck.svg';
+import grey_truck from '../../icons/grey_truck.svg';
+
 class EventCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
             eventType: 'recurring',
-            eventTime: '10am - 12pm'
+            eventTime: '10am - 12pm',
+            eventStatus: 'past'
         };
     }
     render() {
@@ -15,14 +18,30 @@ class EventCard extends Component {
             type = 'Reccuring Pick Up';
         }
         return (
-            <div className="event-container">
-                <h1 className="event-header">{type}</h1>
-                <img
-                    className="truck-icon"
-                    src={green_truck}
-                    alt="green truck"
-                />
-                <p className="event-time">{this.state.eventTime}</p>
+            <div>
+                {this.state.eventStatus === 'future' ? (
+                    <div className="event-container event-container-future">
+                        <h1 className="event-header">{type}</h1>
+
+                        <img
+                            className="truck-icon"
+                            src={green_truck}
+                            alt="green truck"
+                        />
+                        <p className="event-time">{this.state.eventTime}</p>
+                    </div>
+                ) : this.state.eventStatus === 'past' ? (
+                    <div className="event-container event-container-past">
+                        <h1 className="event-header">{type}</h1>
+
+                        <img
+                            className="truck-icon"
+                            src={grey_truck}
+                            alt="grey truck"
+                        />
+                        <p className="event-time">{this.state.eventTime}</p>
+                    </div>
+                ) : null}
             </div>
         );
     }
