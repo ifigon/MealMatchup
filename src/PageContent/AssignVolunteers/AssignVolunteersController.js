5708e0ca8c7e82e2669adcb57dc12b5826dffcf2
zubinchopra <zubinc@uw.edu>
2018-04-03 12:35:12 -0700

    Added AssignVolunteer Files

---
+++ src/PageContent/AssignVolunteers/AssignVolunteersController.js
@@ -0,0 +1,214 @@
+import React, { Component } from 'react';
+import './AssignVolunteers.css';
+import Edit from './Edit';
+import Confirmation from './Confirmation';
+import AssignVolunteersIndex from './AssignVolunteersIndex';
+
+class AssignVolunteersController extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: 0,
+            deliveries: [],
+            selectedDelivery: -1,
+            s1name: '',
+            s1phone: '',
+            s1email: '',
+            s2name: '',
+            s2phone: '',
+            s2email: ''
+        };
+    }
+
+    // TODO: Manually setting the values for now. Values would need to be queried from DB
+
+    componentWillMount() {
+        let deliveryList = [];
+        deliveryList.push({
+            date: '2018-02-28',
+            startTime: '14:00',
+            endTime: '17:00',
+            donatingAgency: {
+                agency: 'Local Point',
+                address: 'Test Address',
+                primaryContact: {
+                    name: 'Alice',
+                    phone: 7739939922
+                }
+            },
+            receivingAgency: {
+                agency: 'Union Gospel Shelter',
+                primaryContact: {
+                    name: 'Bob',
+                    email: 'bob@uniongospel.org',
+                    phone: 1237894560
+                }
+            },
+            delivererGroup: {
+                group: 'Deliverer Test Group',  // uid-key of deliverer-group
+                deliverers: [
+                    {
+                        name: 'Alice',
+                        email: 'alice@uw.edu',
+                        phone: 1237894560
+                    },
+                    {
+                        name: 'Chris',
+                        email: 'chris@uw.edu',
+                        phone: 4561230789
+                    }
+                ]
+            },
+            description: {
+                foodItems: [
+                    {
+                        food: 'Baked beans',
+                        quantity: 15,
+                        unit: 'lb'  // Enums.FoodUnit
+                    },
+                    {
+                        food: 'Bread',
+                        quantity: 4,
+                        unit: 'loaves'  // Enums.FoodUnit
+                    },
+                ]
+            }
+        });
+        deliveryList.push({
+            date: '2018-02-26',
+            startTime: '14:00',
+            endTime: '17:00',
+            donatingAgency: {
+                agency: 'Local Point',
+                address: 'Test Address',
+                primaryContact: {
+                    name: 'Alice',
+                    phone: 7739939922
+                }
+            },
+            receivingAgency: {
+                agency: 'Union Gospel Shelter',
+                primaryContact: {
+                    name: 'Bob',
+                    email: 'bob@uniongospel.org',
+                    phone: 1237894560
+                }
+            },
+            delivererGroup: {
+                group: 'Deliverer Test Group',  // uid-key of deliverer-group
+                deliverers: [
+                    
+                ]
+            },
+            description: {
+                foodItems: [
+                    {
+                        food: 'Baked beans',
+                        quantity: 15,
+                        unit: 'lb'  // Enums.FoodUnit
+                    },
+                    {
+                        food: 'Bread',
+                        quantity: 4,
+                        unit: 'loaves'  // Enums.FoodUnit
+                    },
+                ]
+            }
+        });
+        this.setState({
+            deliveries: deliveryList
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+
+                {this.showStep()}
+
+            </div>
+        );
+
+    }
+
+    handleConfirmClick(s1name, s1phone, s1email, s2name, s2phone, s2email) {
+        this.setState({
+            step: 2,
+            s1name: s1name,
+            s1phone: s1phone,
+            s1email: s1email,
+            s2name: s2name,
+            s2phone: s2phone,
+            s2email: s2email
+        });
+    }
+
+    handleCloseClick() {
+        this.setState({
+            step: 1
+        });
+    }
+
+    handleEditClick(e) {
+        this.setState({
+            step: 1,
+            selectedDelivery: e.target.id
+        });
+    }
+
+    handleCancelClick() {
+        this.setState({
+            step: 0
+        });
+    }
+
+    handleSave() {
+        this.setState({
+            step: 0
+        });
+    }
+
+    showStep() {
+        switch(this.state.step) {
+
+        case 0:
+            return (
+                <AssignVolunteersIndex 
+                    handleEditClick={this.handleEditClick.bind(this)}
+                    deliveries={this.state.deliveries}
+                />
+            );
+
+        case 1:
+            return (
+                <Edit 
+                    delivery={this.state.deliveries[this.state.selectedDelivery]}
+                    handleConfirmClick={this.handleConfirmClick.bind(this)}
+                    handleCancelClick={this.handleCancelClick.bind(this)}
+                />
+            );
+        case 2:
+            return (
+                <Confirmation
+                    handleCloseClick={this.handleCloseClick.bind(this)}
+                    handleSave={this.handleSave.bind(this)}
+                    delivery={this.state.deliveries[this.state.selectedDelivery]}
+                    s1name={this.state.s1name}
+                    s1phone={this.state.s1phone}
+                    s1email={this.state.s1email}
+                    s2name={this.state.s2name}
+                    s2phone={this.state.s2phone}
+                />
+            );
+        default:
+            return (
+                <div />
+            );
+
+        }
+
+    }
+}
+
+export default AssignVolunteersController;
---
+++ src/PageContent/AssignVolunteers/Edit.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class Edit extends Component {
+
+    constructor(props) {
+        super(props);
+        let date = new Date(this.props.delivery.date);
+        this.state = {
+            day: moment(date).format('dddd'),
+            date: moment(date).format('l'),
+            from: this.props.delivery.startTime,
+            to: this.props.delivery.endTime,
+            donatingAgency: this.props.delivery.donatingAgency.agency,
+            receivingAgency: this.props.delivery.receivingAgency.agency,
+            s1name: '',
+            s1phone: '',
+            s1email: '',
+            s2name: '',
+            s2phone: '',
+            s2email: '',
+        };
+    }
+
+    componentDidMount() {
+        this.checkStudents(this.props);
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div className="date-container">
+                    {this.state.day} {this.state.date} Pick-up between {this.state.from} - {this.state.to}
+                </div>
+                <div className="content-container">
+                    <h5 className="location" id="top"><i className="fas fa-circle circle-marker" /><span className="spacing" />{this.state.donatingAgency}</h5>
+                    <i className="fas fa-circle ellipses" />
+                    <i className="fas fa-circle ellipses" />
+                    <i className="fas fa-circle ellipses" />
+                    <h5 className="location" id="bottom"><i className="fas fa-map-marker-alt map-marker" /><span className="spacing" />{this.state.receivingAgency}</h5>
+                </div>
+                <div className="form-container">
+                    <form onSubmit={this.handleConfirmClick.bind(this)}>
+
+                        <label className="label-component">Student Deliverers</label>
+
+                        <div className="form-parent">
+
+                            <div className="form-child">
+                                <label className="label-component details">Student 1</label><br />
+                                <input name="name" type="text" className="form-input" onChange={this.handleNameOne.bind(this)} value={this.state.s1name} required/><br />
+                                <label className="label-component details">Phone</label><br />
+                                <input name="phone" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="xxx-xxx-xxxx" className="form-input" onChange={this.handlePhoneOne.bind(this)} value={this.state.s1phone} required/><br />
+                                <label className="label-component details">Email</label><br />
+                                <input type="email" className="form-input" onChange={this.handleEmailOne.bind(this)} value={this.state.s1email} required/>
+                            </div>
+                            <div className="form-child">
+                                <label className="label-component details">Student 2</label><br />
+                                <input name="name" type="text" className="form-input" onChange={this.handleNameTwo.bind(this)} value={this.state.s2name} required/><br />
+                                <label className="label-component details">Phone</label><br />
+                                <input name="phone" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="xxx-xxx-xxxx" className="form-input" onChange={this.handlePhoneTwo.bind(this)} value={this.state.s2phone} required/><br />
+                                <label className="label-component details">Email</label><br />
+                                <input type="email" className="form-input" onChange={this.handleEmailTwo.bind(this)} value={this.state.s2email} required/>
+                            </div>
+
+                            <div className="form-child second-row">
+                                <h5 className="label-component" id="info">Email notifications will be sent to all deliverers.</h5>
+                            </div>
+                            <div className="form-child second-row form-buttons-container">
+                                <button type="button" className="form-button" onClick={this.props.handleCancelClick}>Cancel</button>
+                                <button type="submit" className="form-button confirm-button" onSubmit={this.handleConfirmClick.bind(this)}>Confirm</button>
+                            </div>
+
+
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        );
+
+    }
+
+    checkStudents(props) {
+        if(props.delivery.delivererGroup.deliverers[0]) {
+            this.setState({
+                s1name: props.delivery.delivererGroup.deliverers[0].name,
+                s1email: props.delivery.delivererGroup.deliverers[0].email,
+                s1phone: props.delivery.delivererGroup.deliverers[0].phone,
+            });
+            if(props.delivery.delivererGroup.deliverers[1]) {
+                this.setState({
+                    s2name: props.delivery.delivererGroup.deliverers[1].name,
+                    s2email: props.delivery.delivererGroup.deliverers[1].email,
+                    s2phone: props.delivery.delivererGroup.deliverers[1].phone,
+                });
+            }
+        }
+    }
+
+    handleNameOne(e) {
+        this.setState({s1name: e.target.value});
+    }
+
+    handlePhoneOne(e) {
+        this.setState({s1phone: e.target.value});
+    }
+
+    handleEmailOne(e) {
+        this.setState({s1email: e.target.value});
+    }
+
+    handleNameTwo(e) {
+        this.setState({s2name: e.target.value});
+    }
+
+    handlePhoneTwo(e) {
+        this.setState({s2phone: e.target.value});
+    }
+
+    handleEmailTwo(e) {
+        this.setState({s2email: e.target.value});
+    }
+
+    handleConfirmClick(e) {
+        e.preventDefault();
+        this.props.handleConfirmClick(this.state.s1name, this.state.s1phone, this.state.s1email, this.state.s2name, this.state.s2phone, this.state.s2email);
+    }
+
+}
+
+export default Edit;
