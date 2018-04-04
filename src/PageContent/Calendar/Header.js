ee30419f24b4c763832fce6c16fb30c7edd70dd6
Jessica Libman <jlibman@gmail.com>
2018-04-04 12:47:20 -0700

    calendar updates

--- src/PageContent/Calendar/Header.js
+++ src/PageContent/Calendar/Header.js
@@ -1,50 +1,45 @@
 import React, {Component} from 'react';
-import Previous from '../../icons/Previous.svg'
-import Next from '../../icons/Next.svg'
-import moment from 'moment'
+import Calendar from 'react-calendar';
+import './Calendar.css'
 
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            month: null,
-            diff: 0,
-            fDay: null,
-            lDay: null
-            
-        };
-        this.nextMonth = this.nextMonth.bind(this);
-        this.prevMonth = this.prevMonth.bind(this);
     }
 
-    componentWillMount() {
-        this.setState({ month: moment().format('MMMM') });
-    }
+    componentDidMount() {
+        // TODO ADD CIRCLES AROUND EACH TIME FOR "TODAY" INDICATOR
+        var dayTimeTags = document.getElementsByTagName('time');
 
-    nextMonth(e) {
-        e.preventDefault();
-        this.setState({diff: this.state.diff+1})
-        var nMonth = moment().add(this.state.diff,'months').format('MMMM');
-        this.setState({month:nMonth});
-    }
+        for (var i = 0; i < dayTimeTags.length; i++) {
+            // `element` is the element you want to wrap
+            var parent = dayTimeTags[i].parentNode;
+            var wrapper = document.createElement('div');
+            wrapper.className = "calendar-day-wrapper";
 
-    prevMonth(e) {
-        e.preventDefault();
-        this.setState({diff: this.state.diff-1})
-        var pMonth = moment().add(this.state.diff,'months').format('MMMM');
-        this.setState({month:pMonth});
+            // set the wrapper as child (instead of the element)
+            parent.replaceChild(wrapper, dayTimeTags[i]);
+            // set element as child of wrapper
+            wrapper.appendChild(dayTimeTags[i]);
+
+            // dayTimeTags[i].
+            // console.log(dayTimeTags[i].firstChild.data); //second console output
+        }
+
+        var calendarTiles = document.getElementsByClassName("react-calendar__tile");
+        for (var i = 0; i < calendarTiles.length; i++) {
+            var eventsDiv = document.createElement('div');
+            eventsDiv.innerHTML = "event";
+            // TODO APPEND EVENTS TO THIS DIV
+            calendarTiles[i].appendChild(eventsDiv);
+        }
+
     }
 
     render() {
         return (
             <div style={{marginTop: '120px', marginLeft:'250px'}} id="calHeader">
-                <div id="monPrev" onClick={this.prevMonth}>
-                    <img alt="icon" type="image/svg+xml" src={Previous} className="user-icon"/>
-                </div>
-                <div id="monName">{this.state.month}</div>
-                <div id="monNext" onClick={this.nextMonth}>
-                    <img alt="icon" type="image/svg+xml" src={Next} className="user-icon"/>
-                </div>
+                <Calendar />
             </div>
         );
     }
