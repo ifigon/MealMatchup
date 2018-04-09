3e9277364418bc504b10781051f33f47f45ed43e
huangjoyce3 <jhuang3@uw.edu>
2018-04-09 15:25:51 -0700

    dummy data for notifications

--- src/PageContainer.js
+++ src/PageContainer.js
@@ -21,7 +21,48 @@ class PageContainer extends Component {
         this.state = {
             content: props.content,
             showPopUp: false,
-            hover: false
+            hover: false,
+            notifications: [
+                {
+                    type: 'recurring_pickup_request',  // Enums.NotificationType
+                    content: '-L5QoXeC_UrL5tRRED3e'  // key of DeliveryRequest
+                }
+            ],
+            deliveryRequest: {
+                status: 'pending',  // Enums.RequestStatus
+                startTimestamp: 1519826400,  // start date + start hour
+                endTimestamp: 1527688800,    // end date + end hour
+                endCriteria: {
+                    type: 'date',  // Enums.RequestEndCriteriaType
+                    value: '2018-02-09'  // a date for "date" or a number for "num_occurrences"
+                },
+                repeats: 'weekly',	// values in Enums.RequestRepeatType
+                primaryContact: 'dhA03LwTp3cibXVUcb3nQqO34wj1',  // uid-key of a donating-agency-member
+                notes: 'Enter through the back door.',
+                umbrella: 'RheaQY1WxJT03sTPQICFZ4STpfm1',  // uid-key of a umbrella
+                donatingAgency: '-K9HdKlCLjjk_ka82K0s',  // autogen-key of a donating-agency
+                requester: 'Andrea Benson',  // name of a donating-agency-member
+                // receivingAgency & delivererGroups:
+                // The donating agency can choose to request specific receiving agency
+                // and/or deliverer group, or not.
+                // a. If specified, 'requested' field will be the uid-key of that RA
+                // b. It not specified, all RA/DG uids in this umbrella will be added to 'pending'
+                receivingAgency: {
+                    // Depending on the status of the request, this
+                    // map will have 1 out of the 3 possible fields:
+                    // "requested", "pending" (list), "claimed"
+                    requested: 'uGOFJ8NqHjbZhKAYzSZFRs1dSKD3',  // uid-key of a RA
+                },
+                delivererGroup: {
+                    // same as receivingAgency above:
+                    requested: 'R8BAHrxdkfQoAmfWvGa1OJmjQP43',  // uid-key of a DG
+                },
+                requestTimeStamp: 1518753363763,
+                spawnedDeliveries: [
+                    // individual deliveries that were created to fulfill this delivery request
+                    '-L5RkIS0CSPuXpkewaqA',
+                ]
+            }
         };
         
         this.navBarHandler = this.navBarHandler.bind(this);
@@ -79,10 +120,12 @@ class PageContainer extends Component {
                 {this.state.content === PageContent.SETTINGS &&
                     <div style={{marginTop: '120px', marginLeft:'250px'}}>Settings</div>
                 }
+                {/* this only shows notification */}
                 {this.state.hover ? 
                     <RecurringDeliveryRequestNotification clickNotification={this.openPopUp.bind(this)}/>
                     : null
                 }
+                {/* this shows popup card */}
                 {this.state.showPopUp ?
                     <EmergencyDeliveryRequestController closePopUp={this.closePopUp.bind(this)}/>
                     : null    
