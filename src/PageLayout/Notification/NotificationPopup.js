2ed5ec1f54afdd797f02bcf38b623a6f2c646b34
huangjoyce3 <jhuang3@uw.edu>
2018-04-09 15:05:41 -0700

    file change

--- src/PageLayout/Notification/NotificationPopup.js
+++ src/PageLayout/Notification/NotificationPopup.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import truck from '../../../icons/green_truck.svg';
+import truckG from '../../../icons/green_truck.svg';
+import { NotificationType, AccountType } from '../../Enums';
 
 class RecurringDeliveryRequestNotification extends Component {
     render() {
         return (
             <div className="notification-wrapper recurring">
-                <img className="icon" src={truck} alt="icon" /><h1 className="hover">Reoccuring pickup requested</h1>
+                {
+                    this.props.notificationType === NotificationType.RECURRING_PICKUP_REQUEST && 
+                    this.props.account === AccountType.RECEIVING_AGENCY &&
+                    <img className="icon" src={truckG} alt="icon"><h1 className="hover">Recurring Pickup Requested</h1></img>
+                }
                 <div className="buttons notification">
                     <button onClick={this.props.clickNotification} className="claim notification" type="button">View</button>
                 </div>
