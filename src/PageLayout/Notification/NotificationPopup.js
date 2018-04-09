d1d553404e9eb06bfffd245f087801e570e66bf4
huangjoyce3 <jhuang3@uw.edu>
2018-04-09 16:48:57 -0700

    css fix

--- src/PageLayout/Notification/NotificationPopup.js
+++ src/PageLayout/Notification/NotificationPopup.js
@@ -10,12 +10,12 @@ class NotificationPopup extends Component {
                 {
                     this.props.notificationType === NotificationType.RECURRING_PICKUP_REQUEST &&
                     this.props.account === AccountType.RECEIVING_AGENCY || this.props.account === AccountType.DELIVERER_GROUP ?
-                        <div className="notification-wrapper recurring">
-                            <img className="icon" src={truckG} alt="icon"/><h1 className="hover">Recurring pickup requested</h1>
-                            <div className="buttons notification">
+                        <span className="notification-wrapper recurring">
+                            <img className="popup-icon" src={truckG} alt="icon"/><h1 className="hover">Recurring pickup requested</h1>
+                            <div className="notification-buttons notification">
                                 <button onClick={this.props.clickNotification} className="claim notification" type="button">View</button>
                             </div>
-                        </div>
+                        </span>
                         :
                         null
                 }
