669f2b603e57f0447e09bc06a01a2d8382f7bbc4
huangjoyce3 <jhuang3@uw.edu>
2018-04-09 17:34:33 -0700

    sticky popup

--- src/PageLayout/Notification/NotificationPopup.js
+++ src/PageLayout/Notification/NotificationPopup.js
@@ -6,28 +6,28 @@ import './Popup.css';
 class NotificationPopup extends Component {
     render() {
         return (
-            <div className="">
+            <div>
                 {
                     this.props.notificationType === NotificationType.RECURRING_PICKUP_REQUEST &&
                     this.props.account === AccountType.RECEIVING_AGENCY || this.props.account === AccountType.DELIVERER_GROUP ?
-                        <span className="notification-wrapper recurring">
+                        <div className="notification-wrapper recurring arrow-up">
                             <img className="popup-icon" src={truckG} alt="icon"/><h1 className="hover">Recurring pickup requested</h1>
                             <div className="notification-buttons notification">
                                 <button onClick={this.props.clickNotification} className="claim notification" type="button">View</button>
                             </div>
-                        </span>
+                        </div>
                         :
                         null
                 }
                 {
-                    this.props.notificationType === NotificationType.RECURRING_PICKUP_CONFIRMED ?
-                    // this.props.account === AccountType.DONATING_AGENCY_MEMBER ?
-                        <div className="notification-wrapper recurring">
-                            <img className="icon" src={truckG} alt="icon"/><h1 className="hover">A recurring pickup has been claimed.</h1>
-                            <div className="buttons notification">
+                    this.props.notificationType === NotificationType.RECURRING_PICKUP_CONFIRMED &&
+                    this.props.account === AccountType.DONATING_AGENCY_MEMBER ?
+                        <span className="notification-wrapper recurring">
+                            <img className="popup-icon" src={truckG} alt="icon"/><h1 className="hover">A recurring pickup has been claimed</h1>
+                            <div className="notification-buttons notification">
                                 <button onClick={this.props.clickNotification} className="claim notification" type="button">View</button>
                             </div>
-                        </div>
+                        </span>
                         :
                         null
                 }
