da62f6d42d6b59eb314d9a4e7593e32d83141f16
huangjoyce3 <jhuang3@uw.edu>
2018-04-13 20:40:08 -0700

    rm account type check

--- src/PageLayout/Notification/NotificationPopup.js
+++ src/PageLayout/Notification/NotificationPopup.js
@@ -9,27 +9,21 @@ class NotificationPopup extends Component {
             <div>
                 {
                     this.props.notificationType === NotificationType.RECURRING_PICKUP_REQUEST &&
-                    (this.props.account === AccountType.RECEIVING_AGENCY || this.props.account === AccountType.DELIVERER_GROUP) ?
                         <div className="notification-wrapper recurring arrow-up">
                             <img className="popup-icon" src={truckG} alt="icon"/><h1 className="hover">Recurring pickup requested</h1>
                             <div className="notification-buttons notification">
                                 <button onClick={this.props.clickNotification} className="claim notification" type="button">View</button>
                             </div>
                         </div>
-                        :
-                        null
                 }
                 {
                     this.props.notificationType === NotificationType.RECURRING_PICKUP_CONFIRMED &&
-                    this.props.account === AccountType.DONATING_AGENCY_MEMBER ?
                         <span className="notification-wrapper recurring">
                             <img className="popup-icon" src={truckG} alt="icon"/><h1 className="hover">A recurring pickup has been claimed</h1>
                             <div className="notification-buttons notification">
                                 <button onClick={this.props.clickNotification} className="claim notification" type="button">View</button>
                             </div>
                         </span>
-                        :
-                        null
                 }
             </div>
         );
