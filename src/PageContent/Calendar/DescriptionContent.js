0199bb01036dd25151b23b7eca0d8e4111e026f6
sarahfeldmann <sarahf95@gmail.com>
2018-04-14 10:45:33 -0700

    add description

--- src/PageContent/Calendar/DescriptionContent.js
+++ src/PageContent/Calendar/DescriptionContent.js
@@ -8,15 +8,19 @@ import plus from '../../icons/plus-button.svg';
 class DescriptionContent extends Component {
     constructor(props) {
         super(props);
+        let donation = [];
+        if (this.props.delivery.donationDescription) {
+            donation = this.props.delivery.donationDescription;
+        }
         this.state = {
             edit: false,
-            donationObject: this.props.delivery.donationDescription,
+            donationObject: donation,
             accountType: this.props.accountType
         };
+
         this.edit = this.edit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.addRow = this.addRow.bind(this);
-        console.log('account type', this.props.accountType);
     }
     edit() {
         this.setState({
@@ -25,6 +29,7 @@ class DescriptionContent extends Component {
     }
 
     addRow() {
+        this.edit();
         let blankRow = { name: '', amount: 0, unit: '' };
         let currentDonation = this.state.donationObject;
         currentDonation.push(blankRow);
@@ -35,21 +40,23 @@ class DescriptionContent extends Component {
 
     stringifyDonation(donation) {
         let foodList = '';
-        if (donation.length > 0) {
-            foodList +=
-                donation[0].name +
-                ' ' +
-                donation[0].amount +
-                ' ' +
-                donation[0].unit;
-            for (let i = 1; i < donation.length; i++) {
+        if (donation != undefined) {
+            if (donation.length > 0) {
                 foodList +=
-                    ', ' +
-                    donation[i].name +
+                    donation[0].name +
                     ' ' +
-                    donation[i].amount +
+                    donation[0].amount +
                     ' ' +
-                    donation[i].unit;
+                    donation[0].unit;
+                for (let i = 1; i < donation.length; i++) {
+                    foodList +=
+                        ', ' +
+                        donation[i].name +
+                        ' ' +
+                        donation[i].amount +
+                        ' ' +
+                        donation[i].unit;
+                }
             }
         }
         return foodList;
@@ -78,35 +85,39 @@ class DescriptionContent extends Component {
     render() {
         let donation = this.stringifyDonation(this.state.donationObject);
 
-        let editDonation = this.state.donationObject.map((item, index) => {
-            return (
-                <div className="donation-edit-wrapper">
-                    <input
-                        type="text"
-                        className="food"
-                        defaultValue={item.name}
-                        name={index + 'name'}
-                    />
-                    <div className="weight-unit-wrapper">
+        let editDonation = '';
+
+        if (this.state.donationObject.length > 0) {
+            editDonation = this.state.donationObject.map((item, index) => {
+                return (
+                    <div className="donation-edit-wrapper">
                         <input
                             type="text"
-                            className="weight"
-                            defaultValue={item.amount}
-                            name={index + 'amount'}
+                            className="food"
+                            defaultValue={item.name}
+                            name={index + 'name'}
                         />
-                        <select
-                            className="description-unit"
-                            defaultValue={item.unit}
-                            name={index + 'unit'}
-                        >
-                            <option>lbs</option>
-                            <option>loaves</option>
-                            <option>cases</option>
-                        </select>
+                        <div className="weight-unit-wrapper">
+                            <input
+                                type="text"
+                                className="weight"
+                                defaultValue={item.amount}
+                                name={index + 'amount'}
+                            />
+                            <select
+                                className="description-unit"
+                                defaultValue={item.unit}
+                                name={index + 'unit'}
+                            >
+                                <option>lbs</option>
+                                <option>loaves</option>
+                                <option>cases</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-            );
-        });
+                );
+            });
+        }
         return (
             <div className="wrapper">
                 <img className="content-icon" src={groceries} alt="volunteer" />
@@ -120,10 +131,21 @@ class DescriptionContent extends Component {
                         </p>
                     ) : null}
                     {!this.state.edit ? (
-                        <div className="content-details-wrapper">
-                            <p className="content-details description-content">
-                                {donation}
-                            </p>
+                        <div>
+                            {this.state.donationObject.length > 0 ? (
+                                <div className="content-details-wrapper">
+                                    <p className="content-details description-content">
+                                        {donation}
+                                    </p>
+                                </div>
+                            ) : (
+                                <button
+                                    className="add-food"
+                                    onClick={this.addRow}
+                                >
+                                    Add Food Items
+                                </button>
+                            )}{' '}
                         </div>
                     ) : (
                         <div className="content-details-wrapper">
@@ -163,7 +185,8 @@ class DescriptionContent extends Component {
                     {this.state.accountType ===
                         AccountType.DONATING_AGENCY_MEMBER &&
                     !this.state.edit &&
-                    this.props.futureEvent ? (
+                    this.props.futureEvent &&
+                    this.state.donationObject.length > 0 ? (
                             <button
                                 type="button"
                                 className="edit-button"
