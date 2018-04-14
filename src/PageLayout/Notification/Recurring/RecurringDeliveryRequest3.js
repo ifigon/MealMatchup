5fa91d0243af78633e762981cea515c09701d01c
huangjoyce3 <jhuang3@uw.edu>
2018-04-14 16:14:28 -0700

    rm view on calendar button

--- src/PageLayout/Notification/Recurring/RecurringDeliveryRequest3.js
+++ src/PageLayout/Notification/Recurring/RecurringDeliveryRequest3.js
@@ -12,11 +12,13 @@ class RecurringDeliveryRequest3 extends Component {
                         it is confirmed by all parties and scheduled.</h1>
                     <p><span className="to-from">From:</span> Local Point      
                     <span className="right to-from">To:</span> Seattle Union Gospel Mission <br/>
-                Start Date: Thursday, 11/21/2018
-                10 Pickups requested for every Thursday
-                Pickup between 10 am -12 pm</p>
+                    {/* TODO: use timestamp for the following info */}
+                        Start Date: Thursday, 11/21/2018
+                        10 Pickups requested for every Thursday
+                        Pickup between 10 am -12 pm</p>
+                    <p id="modal-cal">The delivery is now on your calendar</p>
                     <div className="popup-buttons">
-                        <button onClick={this.props.close} className="claim end" type="button">View on calendar</button>
+                        {/* TODO: View on calendar button */}
                     </div>
                 </div>
             </div>
