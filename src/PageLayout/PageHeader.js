c60b84df301a16006ef1b2014b3e10d700a9ffaa
sarahfeldmann <sarahf95@gmail.com>
2018-04-15 02:12:47 -0700

    fix nav bug

--- src/PageLayout/PageHeader.js
+++ src/PageLayout/PageHeader.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './PageHeader.css';
 import notification from '../icons/notification.svg';
 import NotificationBadge from 'react-notification-badge';
-import {Effect} from 'react-notification-badge';
+import { Effect } from 'react-notification-badge';
 import Notification from './Notification/Notification';
 
 class PageHeader extends Component {
@@ -43,7 +43,7 @@ class PageHeader extends Component {
         });
     }
 
-    openPopUp(index){
+    openPopUp(index) {
         // TODO: backend populate notification popup info based on index
         this.setState({
             showPopUp: true,
@@ -51,48 +51,65 @@ class PageHeader extends Component {
         });
     }
 
-    closePopUp(){
+    closePopUp() {
         this.setState({
             showPopUp: false
         });
     }
 
-    notificationClicked(){
-        this.setState((prevState) => {
-            return {notificationClicked: !prevState.notificationClicked};
+    notificationClicked() {
+        this.setState(prevState => {
+            return { notificationClicked: !prevState.notificationClicked };
         });
     }
 
     render() {
-        return(
+        return (
             <div className="page-container">
                 <div className="flex">
                     <div className="header-title">
-                        <img src={this.props.logo} className="page-logo" alt="icon"/>
+                        <img
+                            src={this.props.logo}
+                            className="page-logo"
+                            alt="icon"
+                        />
                         <p className="page-title">{this.props.title}</p>
                     </div>
-                    <div style={{height: '0px', marginTop: '40px'}}>
-                        <NotificationBadge 
-                            count={this.state.notifications.length} 
+                    <div style={{ height: '0px', marginTop: '40px' }}>
+                        <NotificationBadge
+                            count={this.state.notifications.length}
                             effect={Effect.SCALE}
                             frameLength={15.0}
-                            style={{position: 'inherit', left: '20px', top: '-10px'}}/>
-                        <img onClick={this.notificationClicked} src={notification} className="notification-icon" alt="icon"/>
+                            style={{
+                                position: 'inherit',
+                                left: '20px',
+                                top: '-10px'
+                            }}
+                        />
+                        <img
+                            onClick={this.notificationClicked}
+                            src={notification}
+                            className="notification-icon"
+                            alt="icon"
+                        />
                     </div>
                 </div>
                 <div className="popup-flex">
                     {/* this only shows notification */}
                     {this.state.notificationClicked &&
                         this.state.notifications.map((notification, i) => {
-                            return !notification.claimed && 
-                                <Notification
-                                    key={i}
-                                    index={i}
-                                    notificationType={notification.type} 
-                                    account={this.props.account.accountType}
-                                    clickNotification={this.openPopUp}/>;
-                        })
-                    }
+                            return (
+                                !notification.claimed && (
+                                    <Notification
+                                        key={i}
+                                        index={i}
+                                        notificationType={notification.type}
+                                        account={this.props.account.accountType}
+                                        clickNotification={this.openPopUp}
+                                    />
+                                )
+                            );
+                        })}
                 </div>
             </div>
         );
