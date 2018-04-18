f33d1567a7e499b588c6ce42ddbfed2eec144e99
huangjoyce3 <jhuang3@uw.edu>
2018-04-18 01:22:41 -0700

    rm onClick from form

--- src/PageLayout/Notification/Recurring/EnterPrimaryContact.js
+++ src/PageLayout/Notification/Recurring/EnterPrimaryContact.js
@@ -5,17 +5,19 @@ class EnterPrimaryContact extends Component {
     saveInfo(e){
         e.preventDefault();
         // TODO: Save data
+
+        this.props.savePrimaryContact();
     }
     render() {
         return (
             <div className="modal-wrapper-contact">
                 <h1>Primary Contact for Receiving Delivery</h1>
-                <form className="primary-contact" onSubmit={this.saveInfo}>
+                <form className="primary-contact" onSubmit={this.saveInfo.bind(this)}>
                     <input name="primaryName" type="text" id="primaryName" className="form-component" placeholder="Name" required />
                     <input name="primaryEmail" type="email" id="primaryEmail" className="form-component" placeholder="Email" required/>
                     <input name="primaryPhone" type="tel" pattern={StringFormat.PHONE} className="form-component" placeholder="555-555-5555" id="primaryPhone" required />
                     <div className="popup-buttons">
-                        <button onClick={this.props.savePrimaryContact} className="claim" type="submit">Save</button>
+                        <button className="claim" type="submit">Save</button>
                     </div>
                 </form>
                 
