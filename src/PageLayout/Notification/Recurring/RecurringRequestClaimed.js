d2d12498131efe5785318400a9e0d92ab23a3d20
huangjoyce3 <jhuang3@uw.edu>
2018-04-17 19:32:37 -0700

    adding PickupDetails whereever makes sense

--- src/PageLayout/Notification/Recurring/RecurringRequestClaimed.js
+++ src/PageLayout/Notification/Recurring/RecurringRequestClaimed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import truck from '../../../icons/green_truck.svg';
+import PickupDetails from '../../../PageContent/Summary/PickupDetails';
 
 class RecurringRequestClaimed extends Component {
     // TODO: add pick up to calendar
@@ -11,11 +12,10 @@ class RecurringRequestClaimed extends Component {
                     <h1> Recurring pickup is claimed. We will notify you when 
                         it is confirmed by all parties and scheduled.</h1>
                     <p><span className="to-from">From:</span> Local Point      
-                    <span className="right to-from">To:</span> Seattle Union Gospel Mission <br/>
-                    {/* TODO: use timestamp for the following info */}
-                        Start Date: Thursday, 11/21/2018
-                        10 Pickups requested for every Thursday
-                        Pickup between 10 am -12 pm</p>
+                    <span className="right to-from">To:</span> Seattle Union Gospel Mission <br/></p>
+                    <div className="pickup-details">
+                        <PickupDetails request={this.props.details} />
+                    </div>
                     <p id="modal-cal">The delivery is now on your calendar</p>
                     <div className="popup-buttons">
                         {/* TODO: View on calendar button */}
--- src/PageLayout/Notification/Recurring/RecurringRequestDisclaimer.js
+++ src/PageLayout/Notification/Recurring/RecurringRequestDisclaimer.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import truck from '../../../icons/green_truck.svg';
+import PickupDetails from '../../../PageContent/Summary/PickupDetails';
 
 class RecurringRequestDisclaimer extends Component {
     render() {
         return (
             <div className="modal-wrapper">
-                
                 <img className="icon" src={truck} alt="icon" />
                 <div className="modal-left-align">
                     <h1>Are you sure you want to claim this pickup?</h1>
                     <h2 className="warning">Once a pickup is claimed it cannot be cancelled.</h2>
-                    <p><span className="to-from">From:</span> Local Point      <span className="right to-from">To:</span> Seattle Union Gospel Mission <br/>
-                        {/* TODO: Backend replace info with timestamp */}
-                    Start Date: Thursday, 11/21/2018
-                    10 Pickups requested for every Thursday
-                    Pickup between 10 am -12 pm</p>
+                    <p><span className="to-from">From:</span> Local Point  </p><span className="to-from">To:</span> Seattle Union Gospel Mission <br/>
+                    <div className="pickup-details">
+                        <PickupDetails request={this.props.details} />
+                    </div>
                     <div className="popup-buttons">
                         <button onClick={this.props.nextStep} className="claim" type="button">Confirm</button> <button onClick={this.props.close} className="reject" type="button">Cancel</button>
                     </div>
