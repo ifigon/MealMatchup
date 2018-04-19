25c8d001505f295d80ae6a08f041547d92a6425f
huangjoyce3 <jhuang3@uw.edu>
2018-04-18 21:22:35 -0700

    adding new notification types

--- src/PageLayout/Notification/NotificationMap.js
+++ src/PageLayout/Notification/NotificationMap.js
@@ -1,13 +1,29 @@
 import { NotificationType } from '../../Enums.js';
-import truckG from '../../icons/green_truck.svg';
+import truckGreen from '../../icons/green_truck.svg';
+import truckGrey from '../../icons/grey_truck.svg';
 
 export const NotificationMap = {
     [NotificationType.RECURRING_PICKUP_REQUEST]: {
-        iconSrc: truckG,
+        iconSrc: truckGreen,
         msg: 'Recurring pickup requested'
     },
     [NotificationType.RECURRING_PICKUP_CONFIRMED]: {
-        iconSrc: truckG,
+        iconSrc: truckGreen,
         msg: 'A recurring pickup has been claimed'
+    },
+    [NotificationType.RECURRING_PICKUP_REJECTED_RA]: {
+        iconSrc: truckGrey,
+        msg: 'A recurring pickup was rejected',
+        detailMsg: 'Unfortunately, this recurring pickup was rejected by all notified shelters.'
+    },
+    [NotificationType.RECURRING_PICKUP_REJECTED_DG]: {
+        iconSrc: truckGrey,
+        msg: 'A recurring pickup was rejected',
+        detailMsg: 'Unfortunately, this recurring pickup was rejected by all participating student groups.'
+    },
+    [NotificationType.RECURRING_PICKUP_UNAVAILABLE]: {
+        iconSrc: truckGrey,
+        msg: 'A recurring pickup has been claimed',
+        detailMsg: 'Unfortunately, no participating shelter is available during the requested time.'
     }
 };
