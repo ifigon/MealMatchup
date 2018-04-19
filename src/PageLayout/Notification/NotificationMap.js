754233322a47a2857edf32ac375c39bc76cf17d9
huangjoyce3 <jhuang3@uw.edu>
2018-04-18 23:05:11 -0700

    category in notificationMap

--- src/PageLayout/Notification/NotificationMap.js
+++ src/PageLayout/Notification/NotificationMap.js
@@ -1,4 +1,4 @@
-import { NotificationType } from '../../Enums.js';
+import { NotificationType,NotificationCategory } from '../../Enums.js';
 import truckGreen from '../../icons/green_truck.svg';
 import truckGrey from '../../icons/grey_truck.svg';
 
@@ -6,30 +6,35 @@ export const NotificationMap = {
     [NotificationType.RECURRING_PICKUP_REQUEST]: {
         iconSrc: truckGreen,
         msg: 'Recurring pickup requested',
-        color: 'green'
+        color: 'green',
+        category: NotificationCategory.RECURRING_PICKUP
     },
     [NotificationType.RECURRING_PICKUP_CONFIRMED]: {
         iconSrc: truckGreen,
         msg: 'A recurring pickup has been confirmed and scheduled',
         detailMsg: 'sdsf',
-        color: 'green'
+        color: 'green',
+        category: NotificationCategory.RECURRING_PICKUP
     },
     [NotificationType.RECURRING_PICKUP_REJECTED_RA]: {
         iconSrc: truckGrey,
         msg: 'A recurring pickup was rejected',
         detailMsg: 'Unfortunately, this recurring pickup was rejected by all notified shelters.',
-        color: 'grey'
+        color: 'grey',
+        category: NotificationCategory.RECURRING_PICKUP
     },
     [NotificationType.RECURRING_PICKUP_REJECTED_DG]: {
         iconSrc: truckGrey,
         msg: 'A recurring pickup was rejected',
         detailMsg: 'Unfortunately, this recurring pickup was rejected by all participating student groups.',
-        color: 'grey'
+        color: 'grey',
+        category: NotificationCategory.RECURRING_PICKUP
     },
     [NotificationType.RECURRING_PICKUP_UNAVAILABLE]: {
         iconSrc: truckGrey,
         msg: 'No available shelters for recurring pickup',
         detailMsg: 'Unfortunately, no participating shelter is available during the requested time.',
-        color: 'grey'
+        color: 'grey',
+        category: NotificationCategory.RECURRING_PICKUP
     }
 };
