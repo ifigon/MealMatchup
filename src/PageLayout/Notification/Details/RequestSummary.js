435455d34bb7e30378431adc84e87a9c3357846e
sarahfeldmann <sarahf95@gmail.com>
2018-05-02 13:56:48 -0700

    fix lint

--- src/PageLayout/Notification/Details/RequestSummary.js
+++ src/PageLayout/Notification/Details/RequestSummary.js
@@ -3,6 +3,7 @@ import RequestTime from './RequestTime';
 import truck from '../../../icons/green_truck.svg';
 import Map from '../../../Map/Map.js';
 
+// fix lint
 class RequestSummary extends React.Component {
     render() {
         const { details, title } = this.props;
