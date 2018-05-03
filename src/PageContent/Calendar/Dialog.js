2f453571a771cb4abf491bedc8ca59f531ad5f3d
leontaolong <longt8@uw.edu>
2018-05-03 16:08:08 -0700

    Add background overlay on calendar dialog

--- src/PageContent/Calendar/Dialog.js
+++ src/PageContent/Calendar/Dialog.js
@@ -6,20 +6,24 @@ import DialogContent from './DialogContent';
 class Dialog extends Component {
     render() {
         return (
-            <dialog className="event-dialog" open>
-                <Header
-                    closeDialog={this.props.closeDialog}
-                    startTime={this.props.delivery.startTimestamp}
-                    endTime={this.props.delivery.endTimestamp}
-                    futureEvent={this.props.futureEvent}
-                    accountType={this.props.account.accountType}
-                />
-                <DialogContent
-                    futureEvent={this.props.futureEvent}
-                    delivery={this.props.delivery}
-                    account={this.props.account}
-                />
-            </dialog>
+            <div className="dialog-wrapper">
+                {/* close dialog if anywhere outside of dialog is clicked */}
+                <div className="dialog-overlay" onClick={this.props.closeDialog}></div> 
+                <dialog className="event-dialog" open>
+                    <Header
+                        closeDialog={this.props.closeDialog}
+                        startTime={this.props.delivery.startTimestamp}
+                        endTime={this.props.delivery.endTimestamp}
+                        futureEvent={this.props.futureEvent}
+                        accountType={this.props.account.accountType}
+                    />
+                    <DialogContent
+                        futureEvent={this.props.futureEvent}
+                        delivery={this.props.delivery}
+                        account={this.props.account}
+                    />
+                </dialog>
+            </div>
         );
     }
 }
