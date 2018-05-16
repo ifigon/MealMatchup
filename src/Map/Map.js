ec8f17f6f89530d33711620d371e873fee0d24b3
sarahfeldmann <sarahf95@gmail.com>
2018-05-16 10:52:17 -0700

    fix css

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -8,20 +8,20 @@ const GoogleMap_API_KEY = 'AIzaSyBhUNXr9HGzNW1k0Va7EGeyFsJqUSlkwCU';
 
 const Marker = ({ text }) => (
     <div>
-        <div className="pin bounce"></div>
-        <div className='pulse'></div>
+        <div className="pin bounce" />
+        <div className="pulse" />
     </div>
 );
 
-class Map extends Component{
-    constructor(props){
+class Map extends Component {
+    constructor(props) {
         super(props);
-        
-        this.state  = {
-            center: {lat: 0, lng: 0}, // initial marker state
+
+        this.state = {
+            center: { lat: 0, lng: 0 }, // initial marker state
             zoom: 15,
             validAddress: true,
-            address: '',
+            address: ''
         };
     }
 
@@ -45,12 +45,12 @@ class Map extends Component{
             response => {
                 this.setState({
                     center: response.results[0].geometry.location,
-                    address: address,
+                    address: address
                 });
             },
             error => {
-                this.setState((prevState) => {
-                    return {validAddress: !prevState.validAddress};
+                this.setState(prevState => {
+                    return { validAddress: !prevState.validAddress };
                 });
             }
         );
@@ -63,33 +63,56 @@ class Map extends Component{
             marginLeft: this.props.marginLeft,
             marginRight: this.props.marginRight,
             marginTop: this.props.marginTop,
-            marginBottom: this.props.marginBottom
+            marginBottom: this.props.marginBottom,
+            display: this.props.display
         };
         return (
-            <div className='google-map' style={style}>
-                {this.state.validAddress ?
+            <div className="google-map" style={style}>
+                {this.state.validAddress ? (
                     <GoogleMap
                         bootstrapURLKeys={{
-                            key: GoogleMap_API_KEY,
+                            key: GoogleMap_API_KEY
                         }}
-                        center={ this.state.center }
-                        zoom={ this.state.zoom }>
+                        center={this.state.center}
+                        zoom={this.state.zoom}
+                    >
                         <Marker
                             latLng={this.state.center}
                             lat={this.state.center.lat}
                             lng={this.state.center.lng}
                         />
                     </GoogleMap>
-                    :
+                ) : (
                     <div className="error">Unable to load map</div>
-                }
+                )}
                 {/* Prompts user to open maps on their phone or shows link to Google Maps */}
-                {
-                    isMobile() ?
-                        <a id="ms-address" href={'http://maps.apple.com/?q=' + this.state.center.lat + ',' + this.state.center.lng} target="_blank">{this.state.address}</a>
-                        :
-                        <a id="ms-address-desktop" href={'http://maps.google.com/maps?saddr=' + this.state.center.lat + ',' + this.state.center.lng} target="_blank">{this.state.address}</a>
-                }
+                {isMobile() ? (
+                    <a
+                        id="ms-address"
+                        href={
+                            'http://maps.apple.com/?q=' +
+                            this.state.center.lat +
+                            ',' +
+                            this.state.center.lng
+                        }
+                        target="_blank"
+                    >
+                        {this.state.address}
+                    </a>
+                ) : (
+                    <a
+                        id="ms-address-desktop"
+                        href={
+                            'http://maps.google.com/maps?saddr=' +
+                            this.state.center.lat +
+                            ',' +
+                            this.state.center.lng
+                        }
+                        target="_blank"
+                    >
+                        {this.state.address}
+                    </a>
+                )}
             </div>
         );
     }
--- src/PageContent/PendingAccounts/VerifyAccount.js
+++ src/PageContent/PendingAccounts/VerifyAccount.js
@@ -1,31 +1,84 @@
 import React, { Component } from 'react';
 import './VerifyAccount.css';
 import PendingAccountsListItem from './PendingAccountListItem';
+import truck from '../../icons/red_truck.svg';
+import Map from '../../Map/Map';
 
 class VerifyAccount extends Component {
     render() {
-        let address = (
-            <div className="address-wrapper">
-                <div className="address">
-                    {this.props.agencyAddressData.street1}
-                    {this.props.agencyAddressData.street2},{' '}
-                    {this.props.agencyAddressData.city}{' '}
-                    {this.props.agencyAddressData.state}{' '}
-                    {this.props.agencyAddressData.zipcode}
-                </div>
-                <div className="officeNo">
-                    Office: {this.props.agencyAddressData.officeNo}
-                </div>
-                <div className="deliveryNote">{this.props.deliveryNote}</div>
-            </div>
-        );
         return (
             <div className="verify-accounts-wrapper">
                 <h1 className="verify-header">
                     {this.props.accountType} Account Request
                 </h1>
-                <h2 className="subheader">{this.props.agencyName}</h2>
-                {address}
+                <div className="verify-map-wrapper">
+                    <Map
+                        address={this.props.agencyAddressData}
+                        height={'200px'}
+                        width={'300px'}
+                        marginRight={'30px'}
+                        marginTop={'0px'}
+                        marginBottom={'0px'}
+                        marginLeft={'10px'}
+                        display={'inline-block'}
+                    />
+                </div>
+                <div className="address-wrapper">
+                    <h2 className="subheader">{this.props.agencyName}</h2>
+                    <div>
+                        <div className="address">
+                            {this.props.agencyAddressData.street1}
+                            {this.props.agencyAddressData.street2},{' '}
+                            {this.props.agencyAddressData.city}{' '}
+                            {this.props.agencyAddressData.state}{' '}
+                            {this.props.agencyAddressData.zipcode}
+                        </div>
+                        <div className="officeNo">
+                            Office: {this.props.agencyAddressData.officeNo}
+                        </div>
+                        <div className="deliveryNote">
+                            {this.props.deliveryNote}
+                        </div>
+                    </div>
+                </div>
+                <div className="agency-wrapper">
+                    <h2 className="subheader">Agency Contact</h2>
+                    <div className="contact">
+                        <div className="contact-value">
+                            {' '}
+                            {this.props.primaryContact}
+                        </div>
+                        <div className="contact-value">
+                            {' '}
+                            {this.props.primaryContactData.position}
+                        </div>
+                        <div className="contact-value gray-contact">
+                            {' '}
+                            {this.props.primaryContactData.email}
+                        </div>
+                        <div className="contact-value gray-contact">
+                            {' '}
+                            {this.props.primaryContactData.phone}
+                        </div>
+                    </div>
+                </div>
+                {this.props.emergencyPickup && (
+                    <div className="emergency-pickup-wrapper">
+                        <img
+                            className="verify-truck-icon"
+                            src={truck}
+                            alt="truck"
+                        />
+                        <div className="verify-emergency-pickup-content">
+                            <h2 className="subheader emergency-pickup-subheader">
+                                Emergency Pickup Activated
+                            </h2>
+                            <div className="emergency-pickup-note">
+                                This agency can pick up in times of emergency.{' '}
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         );
     }
