2bd502564dbbe01d5463b1a8d6388343cb214be9
sarahfeldmann <sarahf95@gmail.com>
2018-06-13 14:42:18 -0700

    add accept

--- src/PageContent/PendingAccounts/ConfirmActivate.js
+++ src/PageContent/PendingAccounts/ConfirmActivate.js
@@ -5,31 +5,34 @@ import close from '../../icons/cross-out.svg';
 class ConfirmActivate extends Component {
     render() {
         return (
-            <div className="confirm-activate-wrapper">
-                <h1 className="confirmation-message">
-                    Are you sure you want to accept {this.props.agencyName}{' '}
-                    account? Their account will be fully activated.
-                </h1>
-                <div className="close-dialog">
-                    <img
-                        className="close-x-confirm"
-                        src={close}
-                        onClick={this.props.closeDialog}
-                        alt="close"
-                    />
-                </div>
-                <div className="confirm-button-wrapper" />
-                <div
-                    onClick={this.props.confirmAccept}
-                    className="verify-accept verify-button"
-                >
-                    Confirm
-                </div>
-                <div
-                    onClick={this.props.back}
-                    className="verify-reject verify-button"
-                >
-                    Back
+            <div className="overlay-wrapper" onClick={this.props.back}>
+                <div className="confirm-activate-wrapper">
+                    <h1 className="confirmation-message">
+                        Are you sure you want to accept {this.props.agencyName}{' '}
+                        account? Their account will be fully activated.
+                    </h1>
+                    <div className="close-dialog">
+                        <img
+                            className="close-x-confirm"
+                            src={close}
+                            onClick={this.props.closeDialog}
+                            alt="close"
+                        />
+                    </div>
+                    <div className="confirm-button-wrapper">
+                        <div
+                            onClick={this.props.confirmAccept}
+                            className="verify-accept-popup verify-button-popup"
+                        >
+                            Confirm
+                        </div>
+                        <div
+                            onClick={this.props.back}
+                            className="verify-reject-popup verify-button-popup"
+                        >
+                            Cancel
+                        </div>
+                    </div>
                 </div>
             </div>
         );
