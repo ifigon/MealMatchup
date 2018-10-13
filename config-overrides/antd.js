f061cc6a74c48ee6a9ee511951ff6592dd2d9e97
Ryan Keller <rykeller@uw.edu>
2018-10-12 19:44:41 -0700

    Locked react-scripts version, implemented rewire overrides, less\sass\css modules and antd with plugin-import.

---
+++ config-overrides/antd.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/*
+ANTD THEME:
+Here you can override LESS variables for antd's theme.
+By doing it here, we can override the styles in the node module
+before it's imported, preventing us from needlessly including
+the entire style bundle.
+https://ant.design/docs/react/customize-theme
+*/
+module.exports = {
+  '@primary-color': '#1DA57A'
+}
