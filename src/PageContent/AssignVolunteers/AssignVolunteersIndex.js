969c3ad172735d1da62b46f260f376ae0f2c6f98
ngomichael <michael_ngo@live.com>
2018-10-18 19:37:55 -0700

    updated container widths

--- src/PageContent/AssignVolunteers/AssignVolunteersIndex.js
+++ src/PageContent/AssignVolunteers/AssignVolunteersIndex.js
@@ -11,45 +11,43 @@ class AssignVolunteersIndex extends Component {
             deliveriesExist
         } = this.props;
 
-        //if(Object.keys(deliveries).length === 0) {
-        if(!deliveriesExist) { 
-            return <h3>No Deliveries Found</h3>;
-        } else {
-            return (
-                <div className="avi-container">  
-                    <div className="avi-row">
-                        <div className="container avi-headings">
-                            <div className="avi-detail">
-                                Date
-                            </div>
-                            <div className="avi-detail">
-                                Destination
-                            </div>
-                            <div className="avi-detail">
-                                Volunteers
-                            </div>
-                            <div className="avi-detail">
-                                Assign
-                            </div>
-                            <div className="avi-detail">
-                                Send link to volunteers
-                            </div>
-                        </div>
-                    </div>
-                    {
-                        Object.keys(deliveries)
-                            .sort((dId1, dId2) => deliveries[dId1].startTimestamp - deliveries[dId2].startTimestamp)
-                            .map(deliveryId => 
-                                <AssignOption 
-                                    handleEditClick={handleEditClick} 
-                                    key={deliveryId} 
-                                    delivery={deliveries[deliveryId]} 
-                                    deliveryId={deliveryId}/>)
-                            
-                    } 
-                </div>
-            );
-        }
+    if (!deliveriesExist) {
+      return <h5>No deliveries scheduled</h5>;
+    } else {
+      return (
+        <div className="avi-container">
+          <div className="">
+            <div className="avi-headings">
+              <div className="avi-heading">
+                Date
+              </div>
+              <div className="avi-heading">
+                Destination
+              </div>
+              <div className="avi-heading">
+                Volunteers
+              </div>
+              <div className="avi-heading">
+                Assign
+              </div>
+              <div className="avi-heading">
+                Send link to volunteers
+              </div>
+            </div>
+          </div>
+          {
+            Object.keys(deliveries)
+              .sort((dId1, dId2) => deliveries[dId1].startTimestamp - deliveries[dId2].startTimestamp)
+              .map(deliveryId =>
+                <AssignOption
+                  handleEditClick={handleEditClick}
+                  key={deliveryId}
+                  delivery={deliveries[deliveryId]}
+                  deliveryId={deliveryId} />)
+
+          }
+        </div>
+      );
     }
   }
 }
