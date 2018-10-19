eb8fdf61b8332ea335bc63fca0564f95c0df8323
ngomichael <michael_ngo@live.com>
2018-10-18 22:04:04 -0700

    formatted with prettier

--- src/PageContent/AssignVolunteers/AssignVolunteersIndex.js
+++ src/PageContent/AssignVolunteers/AssignVolunteersIndex.js
@@ -16,36 +16,29 @@ class AssignVolunteersIndex extends Component {
     } else {
       return (
         <div className="avi-container">
-          <div className="">
+          <div>
             <div className="avi-headings">
-              <div className="avi-heading">
-                Date
-              </div>
-              <div className="avi-heading">
-                Destination
-              </div>
-              <div className="avi-heading">
-                Volunteers
-              </div>
-              <div className="avi-heading">
-                Assign
-              </div>
-              <div className="avi-heading">
-                Send link to volunteers
-              </div>
+              <div className="avi-heading">Date</div>
+              <div className="avi-heading">Destination</div>
+              <div className="avi-heading">Volunteers</div>
+              <div className="avi-heading">Assign</div>
+              <div className="avi-heading">Send link to volunteers</div>
             </div>
           </div>
-          {
-            Object.keys(deliveries)
-              .sort((dId1, dId2) => deliveries[dId1].startTimestamp - deliveries[dId2].startTimestamp)
-              .map(deliveryId =>
-                <AssignOption
-                  handleEditClick={handleEditClick}
-                  key={deliveryId}
-                  delivery={deliveries[deliveryId]}
-                  deliveryId={deliveryId} />)
-
-          }
+          {Object.keys(deliveries)
+            .sort(
+              (dId1, dId2) =>
+                deliveries[dId1].startTimestamp -
+                deliveries[dId2].startTimestamp
+            )
+            .map(deliveryId => (
+              <AssignOption
+                handleEditClick={handleEditClick}
+                key={deliveryId}
+                delivery={deliveries[deliveryId]}
+                deliveryId={deliveryId}
+              />
+            ))}
         </div>
       );
     }
