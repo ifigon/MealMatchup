1c763f75cbcf5344eaa9c018a267a3be52d52d0e
Ryan Keller <rykeller@uw.edu>
2018-10-19 18:11:27 -0700

    Rewired CRA config - added basic babel plugins, scss, css modules.

---
+++ config-overrides/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+/*
+WEBPACK OVERRIDES (using react-app-rewired and customize-cra)
+*/
+const path = require('path')
+const { injectBabelPlugin } = require('react-app-rewired')
+const rewireSass = require('react-app-rewire-scss')
+const rewireCssModules = require('@watiko/react-app-rewire-css-modules')
+
+module.exports = function override (config, env) {
+  const isProduction = process.env.NODE_ENV === 'production'
+  /*
+  BABEL:
+  babel-plugin-import:          Required by antd for tree shaking
+  transform-decorators-legacy:  @decorator support, very nice
+  */
+  config = injectBabelPlugin(['transform-decorators-legacy', {}], config)
+
+  /*
+  WEBPACK LOADERS:
+  SASS:                         Awesome stylesheets
+  CSS MODULES:                  Modular, per-component hashed styles
+  */
+  config = rewireSass(config, env)
+  config = rewireCssModules(config, env)
+
+  // Return a customized copy of the CRA webpack config.
+  return config
+}
+
