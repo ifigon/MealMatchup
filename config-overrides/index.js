6777c3e8b5bda80839b574ecb5d2d0d08fe31f56
Ryan Keller <rykeller@uw.edu>
2018-10-19 18:38:10 -0700

    Basic antd setup

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -4,8 +4,10 @@ WEBPACK OVERRIDES (using react-app-rewired and customize-cra)
 */
 const path = require('path')
 const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLess = require('react-app-rewire-less')
 const rewireSass = require('react-app-rewire-scss')
 const rewireCssModules = require('@watiko/react-app-rewire-css-modules')
+const antdTheme = require('./antd')
 
 module.exports = function override (config, env) {
   const isProduction = process.env.NODE_ENV === 'production'
@@ -15,13 +17,19 @@ module.exports = function override (config, env) {
   transform-decorators-legacy:  @decorator support, very nice
   */
   config = injectBabelPlugin(['lodash'])
+  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config)
   config = injectBabelPlugin(['transform-decorators-legacy', {}], config)
 
   /*
   WEBPACK LOADERS:
+  LESS: Used by antd, loader options set the theme colors
   SASS:                         Awesome stylesheets
   CSS MODULES:                  Modular, per-component hashed styles
   */
+  config = rewireLess.withLoaderOptions({
+     modifyVars: antdTheme,
+     javascriptEnabled: true
+   })(config, env)
   config = rewireSass(config, env)
   config = rewireCssModules(config, env)
 
