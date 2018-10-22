4d4c29596a61d5b445798ce93885a4172e8ffa4b
Ryan Keller <rykeller@uw.edu>
2018-10-22 12:46:06 -0700

    Rebased with features/build-infrastructure

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -4,8 +4,10 @@ WEBPACK OVERRIDES (using react-app-rewired and customize-cra)
 */
 const path = require('path')
 const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLess = require('react-app-rewire-less')
 const rewireSass = require('react-app-rewire-scss')
 const rewireCssModules = require('@watiko/react-app-rewire-css-modules')
+const antdTheme = require('./antd')
 
 module.exports = function override (config, env) {
   const isProduction = process.env.NODE_ENV === 'production'
@@ -16,13 +18,19 @@ module.exports = function override (config, env) {
   (NOTE: babel-preset-stage-3 is implicitly loaded by react-geocode as an unmarked peer dependency)
   */
   config = injectBabelPlugin(['lodash', {}], config)
+  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config)
   config = injectBabelPlugin(['transform-decorators-legacy', {}], config)
 
   /*
   WEBPACK LOADERS:
+  LESS: Used by antd, loader options set the theme colors
   SASS:                         Awesome stylesheets
   CSS MODULES:                  Modular, per-component hashed styles
   */
+  config = rewireLess.withLoaderOptions({
+     modifyVars: antdTheme,
+     javascriptEnabled: true
+   })(config, env)
   config = rewireSass(config, env)
   config = rewireCssModules(config, env)
 
