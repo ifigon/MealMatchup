319800f293d8a45811f682f24c65c31e0081ac62
Ryan Keller <rykeller@uw.edu>
2018-11-08 23:11:03 -0800

    Fixed errors being shown prematurely.

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
     handleUnauthorized = (account) => {
         this.setState({
             authenticated: true,
-            signInDenied: true,
+            signInDenied: !!account,
             account: null,
             donatingAgency: null,
         });
