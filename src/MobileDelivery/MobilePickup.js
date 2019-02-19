31ea29649bae481ce70ae1df4eca1c45faed4e21
Ian Figon <ifigon@outlook.com>
2019-02-19 00:38:38 -0800

    almost finished, just need to wrap up filtering and last bit of testing

--- src/MobileDelivery/MobilePickup.js
+++ src/MobileDelivery/MobilePickup.js
@@ -3,6 +3,9 @@ import Map from '../Map/Map';
 import moment from 'moment';
 import './Mobile.css';
 import { DeliveryStatus, StringFormat } from '../Enums';
+import minus from '../icons/minus-button.svg'
+import { objectsAreEqual } from '../utils/Utils';
+
 
 import firebase from '../FirebaseConfig';
 const db = firebase.database();
@@ -11,11 +14,22 @@ class MobilePickup extends React.Component {
     constructor(props){
         super(props); 
         this.onSubmit = this.onSubmit.bind(this);
+        this.addFoodItem = this.addFoodItem.bind(this);
+        this.initialFoodItems = [];
+        this.state = {
+            currentItems : [],
+        }
     }
 
-    componentDidMount() {
-        //const test = await db.ref(this.props.dbRef).child('description').once('value');
-        //console.log(test);
+    async componentDidMount() {
+        let dbRef = db.ref(this.props.dbRef);
+        dbRef.child('description/foodItems').once('value', (deliveryItemsSnap) => {
+            let deliveryItemsVal = deliveryItemsSnap.val();
+            if(deliveryItemsVal !== null) {    // check for initial case where food items never added
+                this.setState({ currentItems : deliveryItemsVal });
+                this.initialFoodItems = deliveryItemsVal;
+            }
+        });    
     }
 
     onSubmit(e){
@@ -25,8 +39,44 @@ class MobilePickup extends React.Component {
             signature: e.target.signature.value,
             timestamp: moment().valueOf(),
         };
-        // write pickedUpInfo and status change to db
-        db.ref(this.props.dbRef).update({ status: DeliveryStatus.PICKED_UP, pickedUpInfo: pickedUpInfo});
+
+        let updates = {};
+        updates['status'] = DeliveryStatus.PICKED_UP;
+        updates['pickedUpInfo'] = pickedUpInfo;
+        if(this.foodItemsChanged()) {
+            updates['description/foodItems'] = this.state.currentItems;
+            updates[`description/updatedBy/${moment().valueOf()}`] = this.props.deliveryObj.delivererGroup;
+        } 
+        db.ref(this.props.dbRef).update(updates);
+    }
+
+    foodItemsChanged() {
+        if(this.state.currentItems.length !== this.initialFoodItems.length) return true;
+        for (let i = 0; i < this.state.currentItems.length; i++) {
+            if (!objectsAreEqual(this.state.currentItems[i], this.initialFoodItems[i])) {
+                return true;
+            }
+        }
+    }
+
+    changeFoodItem = (index, isName) => event => {
+        const newFoodItems = this.state.currentItems.map((item, foodIndex) => {
+            if (index !== foodIndex) return item;
+            return isName ? { ...item, food: event.target.value } : { ...item, quantity: event.target.value } 
+        });
+        this.setState({ currentItems: newFoodItems });
+    }
+
+    addFoodItem() {
+        this.setState({
+            currentItems: this.state.currentItems.concat([{ food: "" , quantity: "0", unit: "lb"}])
+        });
+    }
+
+    deleteFoodItem(index) {
+        this.setState({
+            currentItems: this.state.currentItems.filter((item, itemIndex) => index !== itemIndex)
+        });
     }
 
     render() {
@@ -66,15 +116,26 @@ class MobilePickup extends React.Component {
                             {/* TODO: validate input in less than 32 degrees */}
                             <input name="temp" className="ms-input" type="number" placeholder="Freezer Temperature" required/>
                         </div>
-                        <div id="ms-confirm">
-                            <p className="ms-content-header">Confirmation Signature</p>
-                            <p className="ms-notes">Get a confirmation signature from {this.props.da.primaryContact.name} at {this.props.da.agency} after you pick-up food items at the destination.</p>
-                            {/* TODO: Signature */}
-                            <input name="signature" className="ms-signature" type="text" placeholder="Signature" required/>
-                        </div>
                         <div id="ms-delivery-items">
                             <p className="ms-content-header">Confirm / Input Delivery Items</p>
                             <p className="ms-notes">Make sure that all of the items you're delivering today are inputted with their weights so they can be tracked.</p>
+                            { this.state.currentItems.length > 0 ? 
+                                <ul className="mobile-food-list">
+                                    {this.state.currentItems.map((item, idx) => (
+                                        <li className="mobile-food-item" key={idx}>
+                                            <input type="text" placeholder="Add an item" value={item.food} onChange={this.changeFoodItem(idx, true)} />
+                                            <input type="number" placeholder={idx + 1} value={item.quantity} onChange={this.changeFoodItem(idx, false)} />
+                                            <button type="button" onClick={() => this.deleteFoodItem(idx)}><img src={minus} alt="minus" /></button>
+                                        </li>
+                                    ))}
+                                </ul> :
+                                <p className="mobile-empty-items">It looks like no items have been inputted. Add them below.</p>
+                            }
+                            <button type="button" className="mobile-add-item" onClick={this.addFoodItem} ><img src="/static/media/plus-button.21dea89e.svg" alt="plus" /><span className="mobile-add-item-text">Add New Item</span></button>
+                        </div>
+                        <div id="ms-confirm">
+                            <p className="ms-content-header">Confirmation Signature</p>
+                            <p className="ms-notes">Get a confirmation signature from {this.props.da.primaryContact.name} at {this.props.da.agency} after you pick-up food items at the destination.</p>
                             {/* TODO: Signature */}
                             <input name="signature" className="ms-signature" type="text" placeholder="Signature" required/>
                         </div>
