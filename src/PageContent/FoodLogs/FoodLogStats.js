e0515692bf2617866bf71b711522b17f171fd075
Ian Figon <ifigon@outlook.com>
2019-02-19 11:06:25 -0800

    finished pending a bit more testing

--- src/PageContent/FoodLogs/FoodLogStats.js
+++ src/PageContent/FoodLogs/FoodLogStats.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import './FoodLogStats.css';
+import moment from 'moment';
 
 class FoodLogStats extends Component{
     constructor(props) {
         super(props);
+        this.allTime = 5200;  // arbitrary value to represent "all time" filter, filters for last 5200 weeks/ 100 years
         this.state = {
             foodAggregate: {},
-            weeks: 5200,
+            weeks: this.allTime,        
         }
     }
 
     componentDidMount() {
+        this.updateFood(this.allTime);
+    }
+
+    filterTime(weeksToFilter) {
+        this.setState({weeks: weeksToFilter})
+        this.updateFood(weeksToFilter);
+    }
+
+    updateFood(weeksToFilter) {
         let allFoods = {};
+        let timeCutOff = moment().subtract(weeksToFilter, 'weeks').valueOf();
+
         this.props.deliveries.forEach((delivery) => {
-            if(delivery.description) {
+            if(delivery.description && delivery.endTimestamp > timeCutOff) {
+                console.log(delivery);
                 let items = delivery.description.foodItems;
                 items.forEach((item) => {
                     let food = item.food.toLowerCase();
@@ -26,10 +40,6 @@ class FoodLogStats extends Component{
         this.setState({foodAggregate : allFoods})    
     }
 
-    filterTime(weeksToFilter) {
-        this.setState({weeks: weeksToFilter})
-    }
-
     render() {
         let totalQuantity = 0;
         let foodData = Object.keys(this.state.foodAggregate).map((food, index) => {
@@ -39,10 +49,6 @@ class FoodLogStats extends Component{
                         <div className="food-quantity">{this.state.foodAggregate[food]} lbs.</div>
                     </div>);
         });
-
-        let filterTimeButtons = [
-
-        ]
 
         return(
             <div className="food-log-margin">
@@ -54,7 +60,7 @@ class FoodLogStats extends Component{
                         <button className={this.state.weeks === 8 ? "log-button-active" : ""} onClick={() => this.filterTime(8)}>2 Months</button>
                         <button className={this.state.weeks === 24 ? "log-button-active" : ""} onClick={() => this.filterTime(24)}>6 Months</button>
                         <button className={this.state.weeks === 52 ? "log-button-active" : ""} onClick={() => this.filterTime(52)}>1 Year</button>
-                        <button className={this.state.weeks === 5200 ? "log-button-active" : ""} onClick={() => this.filterTime(5200)}>All Time</button>
+                        <button className={this.state.weeks === 5200 ? "log-button-active" : ""} onClick={() => this.filterTime(this.allTime)}>All Time</button>
                     </div>
                 </div>
                 <div className="food-data">
